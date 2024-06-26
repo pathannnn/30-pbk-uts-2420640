592694086a8d0210be425e15e73b54b4227429d2
Fatih Masrukhan <111848439+pathannnn@users.noreply.github.com>
2024-06-26 12:01:07 +0700

    eror esp

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,37 +1,38 @@
-
-<!-- views/Albums.vue -->
 <template>
-    <Menu />
-    <q-page>
+    <div>
+      <Menu />
+      <q-page>
         <q-list bordered>
-            <q-item v-for="album in albums" :key="album.id" clickable @click="viewPhotos(album.id)">
-                <q-item-section>{{ album.title }}</q-item-section>
-            </q-item>
+          <q-item v-for="album in albums" :key="album.id" clickable @click="viewPhotos(album.id)">
+            <q-item-section>{{ album.title }}</q-item-section>
+          </q-item>
         </q-list>
-    </q-page>
-</template>
-
-<script>
-import { useAlbumStore } from '../stores/albums'
-import { onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import Menu from '../components/Menu.vue'
-
-export default {
+      </q-page>
+    </div>
+  </template>
+  
+  <script>
+  import { useAlbumStore } from '../stores/albums'
+  import { onMounted } from 'vue'
+  import { useRouter } from 'vue-router'
+  import Menu from '../components/Menu.vue'
+  
+  export default {
     components: { Menu },
     setup() {
-        const albumStore = useAlbumStore()
-        const router = useRouter()
-
-        onMounted(() => {
-            albumStore.fetchAlbums()
-        })
-
-        const viewPhotos = (albumId) => {
-            router.push(`/albums/${albumId}`)
-        }
-
-        return { albums: albumStore.albums, viewPhotos }
+      const albumStore = useAlbumStore()
+      const router = useRouter()
+  
+      onMounted(() => {
+        albumStore.fetchAlbums()
+      })
+  
+      const viewPhotos = (albumId) => {
+        router.push({ path: `/albums/${albumId}` })
+      }
+  
+      return { albums: albumStore.albums, viewPhotos }
     }
-}
-</script>
+  }
+  </script>
+  
